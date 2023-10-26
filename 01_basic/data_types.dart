e34d10e333c91d01ec1f6f78417bd788448d969c
Miguel Ardila <miguel_sxe@hotmail.com>
2023-10-26 18:24:32 -0500

    sstring

--- 01_basic/data_types.dart
+++ 01_basic/data_types.dart
@@ -1,10 +1,24 @@
-
 main() {
   // ===== number
-  int a = 10;
+  int a = 10; // entero
 
-  double $b = 30;
-
+  double $b = 30.1; // números decimales
 
   print(a + $b);
+
+  // ===== string
+  String nombre = "Juan";
+  String apellido = "Pérez";
+
+  print(nombre + apellido);
+  String nombreCompleto = "$nombre $apellido";
+
+  String multilinea = '''
+  Hello World!
+  Amy Ardila
+  $nombreCompleto
+  Myson
+  ''';
+
+  print(multilinea);
 }

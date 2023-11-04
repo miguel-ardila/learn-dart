6b6565ec81d13415b5a6a391901fbf1b103480c7
Miguel Ardila <miguel_sxe@hotmail.com>
2023-11-04 11:49:00 -0500

    types - dart

--- 01_basic/data_types.dart
+++ 01_basic/data_types.dart
@@ -21,13 +21,58 @@ main() {
   ''';
 
   print(multilinea);
+
+  // ===== booleano
+  bool esMayorDeEdad = true;
+  bool isNotActive = !esMayorDeEdad; // ! ese simbolo lo convierte en false
+  print(isNotActive);
+
+  // ===== list  listas o arreglos
+  List<int> lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  print(lista);
+  print(lista.length);
+  print(lista[0]);
+  print(lista[5]);
+
+  List<String> villanos = ["Doom", "megatron", "decepticons"];
+  print(villanos);
+  print(villanos.length);
+
+  List<dynamic> villains = [
+    "Doom",
+    "Megatron",
+    "Decepticons",
+    1,
+    2,
+    3,
+    2.3,
+    true
+  ];
+  villains[0] = 'superman';
+  villains.add('Duende verde');
+  print(villains);
+  print(villains.length);
+
+  // ===== sets list,  los sets no tiene duplicados
+  Set<int> set = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  set.add(650);
+  print(set);
+  print(set.length);
+
+  // ===== maps
+  Map<int, String> mapa = {1: "uno", 2: "dos", 3: "tres"};
+  print(mapa);
+  print(mapa.length);
+  print(mapa[1]);
+
+  Map<String, dynamic> familia = {
+    'mama': "Stefanny",
+    'edad': 31,
+    'papa': "Miguel",
+    'edad2': 32,
+    'hija': "Amy",
+    'edad3': 5
+  };
+
+  print(familia);
 }
-
-// Ejemplo 1: URL de una API para solicitudes de red
-String apiEndpoint = 'https://api.ejemplo.com/v1/usuarios';
-
-// Ejemplo 2: Consulta SQL para usar en una base de datos
-String sqlQuery = 'SELECT * FROM productos WHERE precio < 100.00';
-
-// Ejemplo 3: JSON String para serialización/deserialización
-String jsonString = '{"nombre": "Carlos", "edad": 30, "profesion": "Ingeniero de Software"}';

66410e86652c99d261b0466cb9d6541d87e9b61e
Miguel Ardila <miguel_sxe@hotmail.com>
2023-11-05 19:13:33 -0500

    variables

---
+++ 01_basic/variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  var a = 10;
+  final b = 10; //no pueden cambiar su valor pero deja agregar otro valor al final
+  const c = 10; //no pueden cambiar su valor
+
+  a = 30;
+  print(a);
+
+  //ejemplo
+
+  final personasFinal = ['Juan', 'Pedro', 'Lucas'];
+  const personasConst = ['Juan', 'Pedro', 'Lucas'];
+
+  personasFinal.add('Maria');
+  //personasConst.add('Maria');
+
+  print(personasFinal);
+  print(personasConst);
+
+
+}

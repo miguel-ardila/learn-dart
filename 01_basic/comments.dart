ef101957c405037a3f7ee5daea09aff2fb444d16
Miguel Ardila <miguel_sxe@hotmail.com>
2023-11-07 12:00:39 -0500

    comments

---
+++ 01_basic/comments.dart
@@ -0,0 +1,15 @@
+main() {
+  // variable temporal
+  int a = 0;
+  print(a);
+
+  /*  final personas = ['Pedor', 'Juan', 'Pablo'];
+
+  print(personas);  */
+
+
+
+  /// Recibe un [nombre] y se concatena cone le [mensaje]
+  saludar(int nombre, int mensaje) {}
+  print(saludar);
+}

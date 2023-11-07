9f1c609684fd486447763adb234418091fb2ef30
Miguel Ardila <miguel_sxe@hotmail.com>
2023-11-07 12:00:17 -0500

    variables

--- 01_basic/variables.dart
+++ 01_basic/variables.dart
@@ -1,25 +1,37 @@
 void main() {
   var a = 10;
-  final b =
-      10; //no pueden cambiar su valor pero deja agregar otro valor al final
+  final b = 10; //no pueden cambiar su valor pero deja agregar otro valor al final
   const c = 10; //no pueden cambiar su valor
 
   a = 30;
-  print(a);
+  /*  print(a);
+  print(b + c); */
 
   //ejemplo
 
   final personasFinal = ['Juan', 'Pedro', 'Lucas'];
   const personasConst = ['Juan', 'Pedro', 'Lucas'];
 
+  //final List<String> personasFinal = ['Juan', 'Pedro', 'Lucas'];
+  //list<string> personasConst = const ['Juan', 'Pedro', 'Lucas'];
+
   personasFinal.add('Maria');
   //personasConst.add('Maria');
 
-  print(personasFinal);
-  print(personasConst);
+  /* print(personasFinal);
+  print(personasConst); */
 
-  final amy = "Amy";
-  print(amy);
+  //final amy = "Amy";
+  /*  print(amy); */
+
+  // ==== Variable Late, Late inicializa la variable despues
+
+  /* double x = 10.25; */
+  late final double x;
+  x = 10.25;
+  print(x);
+  
+
 
 
 

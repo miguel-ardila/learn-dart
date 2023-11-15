ce8328afce805cd38e70c24cb731fd6db11f1362
Miguel Ardila <miguel_sxe@hotmail.com>
2023-11-14 19:51:29 -0500

    cilco - for

--- 02_basic/for.dart
+++ 02_basic/for.dart
@@ -3,4 +3,27 @@ main() {
     // print('index i: $i');
     print('index i: ${2 + 4}');
   }
+
+  // Leterar una lista
+  List<String> frutas = ['manzana', 'banana', 'naranja'];
+
+  for (String fruta in frutas) {
+    print(fruta);
+  }
+  // Uso de un contador
+   for (int i = 0; i < 10; i++) {
+    print('Número: $i');
+  }
+
+  //  Uso de for para crear una tabla de multiplicar
+  int numero = 5;
+
+  for (int i = 1; i <= 10; i++) {
+    print('$numero x $i = ${numero * i}');
+  }
+
+
+
 }
+
+
